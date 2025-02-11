662660d35de37ada73fcd8b1e3404e90d0af3052
josekayk <batistakayk427@gmail.com>
2025-02-10 21:23:11 -0300

    Add files via upload

---
+++ pagina_principalKAupdate/js/app.js
@@ -0,0 +1,64 @@
+/*function aplicarCores() {
+    
+    const cores = {
+        "Alta": "#CF3F3F",  // Vermelho
+        "Média": "#FFBD3D", // Amarelo
+        "Baixa": "#72C240"  // Verde
+    };
+
+   
+    const elementos = document.querySelectorAll('.etiqueta__prioridade');
+
+   
+    elementos.forEach(elemento => {
+        const prioridade = elemento.textContent.trim(); 
+        if (cores[prioridade]) {
+            elemento.style.backgroundColor = cores[prioridade]; 
+            elemento.style.color = "#fff"; 
+            elemento.style.padding = "5px 10px"; 
+            elemento.style.borderRadius = "5px"; 
+            elemento.style.fontWeight = "bold"; 
+        }
+    });
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Forma do professor:
+
+ /*const tarefas = require('./constantes');  Uma forma de exportar */ 
+
+ import{ TAREFAS } from './constantes';
+ const etiquetasPrioridade = document.querySelectorAll('.etiqueta__prioridade');
+
+etiquetasPrioridade.forEach(prioridade => {
+    if(prioridade.textContent === 'Alta') {
+        prioridade.classList.add('etiqueta--alta')
+        
+    } else if(prioridade.textContent === 'Média') {
+        prioridade.classList.add('etiqueta--media')
+
+    } else if(prioridade.textContent === 'Baixa') {
+        prioridade.classList.add('etiqueta--baixa')
+    }
+
+})
+
+function criarTarefa(){
+    const listaDeTarefas = document.getElementById('listaDeTarefas')
+
+    console.log(listaDeTarefas)
+}
+
+criarTarefa()
---
+++ pagina_principalKAupdate/js/constantes.js
@@ -0,0 +1,11 @@
+export const TAREFAS = [
+    {
+        titulo: 'Revisar código crítico', 
+        descricao: 'Revisar bugs críticos no módulo principal.',
+        prioridade: 'Alta',
+        data: '12/12/2012',
+        responsavel: ['Luisa', 'Fabiano'],
+    }
+]
+
+export const PRIORIDADES = ['Alta', 'Média', 'Baixa'];
